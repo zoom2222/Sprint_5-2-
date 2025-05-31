6c90ecb58beeb600b20aacd9b8b291e1c5bd1797
Oleg Shatohin <metman.oleg@gmail.com>
2025-05-31 17:38:57 +0300

    First commit in Sprint_5

--- Tests/test_login_registration.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-
-
-driver.get("https://stellarburgers.nomoreparties.site/")
-
-pass
